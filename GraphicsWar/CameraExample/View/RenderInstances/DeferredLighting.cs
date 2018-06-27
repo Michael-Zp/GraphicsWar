2b13dc00647736585bf81b7a945e8d1e61cae115
Michael Zappe <mz-171742@AP053108>
2018-06-27 11:57:01 +0200

    Added deferredLighting skeleton

---
+++ GraphicsWar/CameraExample/View/RenderInstances/DeferredLighting.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using Zenseless.HLGL;
+using Zenseless.OpenGL;
+
+namespace GraphicsWar.View.RenderInstances
+{
+    public class DeferredLighting : IRenderInstance, IUpdateResolution
+    {
+        public ITexture2D Output
+        {
+            get
+            {
+                return _renderSurface.Texture;
+            }
+        }
+
+        private IShaderProgram _shader;
+        private IRenderSurface _renderSurface;
+        private byte _fboTexComponentCount;
+        private bool _fboTexFloat;
+        
+        public DeferredLighting(IShaderProgram postProcessShader, byte fboTexComponentCount = 4, bool fboTexFloat = false)
+        {
+            _shader = postProcessShader;
+            _fboTexComponentCount = fboTexComponentCount;
+            _fboTexFloat = fboTexFloat;
+        }
+
+        public void UpdateResolution(int width, int height)
+        {
+            _renderSurface = new FBO(Texture2dGL.Create(width, height, _fboTexComponentCount, _fboTexFloat));
+
+            _shader.Uniform("iResolution", new Vector2(width, height));
+        }
+    }
+}
